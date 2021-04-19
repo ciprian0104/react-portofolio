5ab84e92bb11f1ab2fc4221cf2156a6cccd98af4
ciprian0104 <farazahar14@gmail.com>
2021-04-19 18:50:47 +0300

    Added CNAME

---
+++ build/precache-manifest.da11b5dbdbdfa7eacaf18d3fb8643bdc.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32912cba3918c5501375969ab22d82e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4654b6ea57eaff8e0e56",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "5caf4ed05a5819e866e6",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "a42a624005f7532ff0ce",
+    "url": "/static/css/main.207db4cf.chunk.css"
+  },
+  {
+    "revision": "1e57cf75b33a00c2ceea",
+    "url": "/static/js/2.7a4bfb2d.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.7a4bfb2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4654b6ea57eaff8e0e56",
+    "url": "/static/js/3.f43b0d6a.chunk.js"
+  },
+  {
+    "revision": "5caf4ed05a5819e866e6",
+    "url": "/static/js/4.36df55d1.chunk.js"
+  },
+  {
+    "revision": "a42a624005f7532ff0ce",
+    "url": "/static/js/main.9152a8e6.chunk.js"
+  },
+  {
+    "revision": "eb31526584e0ff4f70cf",
+    "url": "/static/js/runtime-main.6e3fd9ec.js"
+  },
+  {
+    "revision": "a50836b79a21144f1878f703ed410a79",
+    "url": "/static/media/3pillar.a50836b7.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "3c5b90df3cdc1a1af51798bb21a8d2d0",
+    "url": "/static/media/deeplearning.3c5b90df.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b955c17575d82c0f12f89cdafa042881",
+    "url": "/static/media/freelancerLogo.b955c175.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "24d441e4446961605b8c100713122212",
+    "url": "/static/media/hklogo.24d441e4.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "7e02f4edf8ea1669cfd778a96b83ad84",
+    "url": "/static/media/real-estate.7e02f4ed.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "98cf7d6d899badc5d9752a12d6e97209",
+    "url": "/static/media/stanford-logo.98cf7d6d.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "f1ad942683423397b50ad5cd34e4e5dc",
+    "url": "/static/media/trelloLogo.f1ad9426.png"
+  }
+]);
